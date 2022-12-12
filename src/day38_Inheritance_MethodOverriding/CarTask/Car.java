6aa00ff26a6fee67caeb0151cecc1959fae27ff5
Halid Erdur <haliderdur@gmail.com>
2022-12-12 21:53:03 +0000

    day38_Inheritance Continue_MethodOverriding

--- src/day38_Inheritance_MethodOverriding/CarTask/Car.java
+++ src/day38_Inheritance_MethodOverriding/CarTask/Car.java
@@ -9,7 +9,8 @@ public class Car {
     public String color;
     public int miles;
 
-    public Car(String brand, String model, int year, double price, String color, int miles) {
+    public Car(String brand, String model, int year,
+               double price, String color, int miles) {
         this.brand = brand;
         this.model = model;
         this.year = year;
