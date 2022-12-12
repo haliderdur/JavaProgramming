c309beb45e5e13ae953c2c0057d86c442fe05222
Halid Erdur <haliderdur@gmail.com>
2022-12-12 22:12:18 +0000

    day38_Inheritance Continue_MethodOverriding

---
+++ src/day38_Inheritance_MethodOverriding/CarTask/BMW.java
@@ -0,0 +1,16 @@
+package day38_Inheritance_MethodOverriding.CarTask;
+
+public class BMW extends Car {
+
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breakDown() {
+        System.out.println(brand + " " + model + " breaks down in every 200 miles");
+    }
+
+    public void racing() {
+        System.out.println(brand + " " + model + " is a racing car");
+    }
+}
---
+++ src/day38_Inheritance_MethodOverriding/CarTask/Tesla.java
@@ -0,0 +1,12 @@
+package day38_Inheritance_MethodOverriding.CarTask;
+
+public class Tesla extends Car {
+
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autoPilot() {
+        System.out.println(brand + " " + model + " has autopilot");
+    }
+}
