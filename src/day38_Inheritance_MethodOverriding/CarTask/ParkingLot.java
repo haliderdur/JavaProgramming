45c14837326746957dcc512b4b3af48efd6dc456
Halid Erdur <haliderdur@gmail.com>
2022-12-13 01:04:34 +0000

    day38_Inheritance Continue_MethodOverriding

---
+++ src/day38_Inheritance_MethodOverriding/CarTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance_MethodOverriding.CarTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2020, 20000, "red", 123456);
+
+        Tesla tesla = new Tesla("Model S", 2021, 35000, "blue", 15123);
+
+        BMW bmw = new BMW("520", 2022, 40000, "White", 0);
+    }
+
+}
