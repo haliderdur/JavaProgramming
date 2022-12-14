4685f154f7af8f8587d4608c7435163450a34dbd
Halid Erdur <haliderdur@gmail.com>
2022-12-14 00:34:37 +0000

    Created Teacher subClass of Employee

---
+++ src/day38_Inheritance_MethodOverriding/CydeoTask/Teacher.java
@@ -0,0 +1,12 @@
+package day38_Inheritance_MethodOverriding.CydeoTask;
+
+public class Teacher extends Employee {
+    public Teacher(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle() + " " + getName() + " is teaching Java");
+    }
+}
