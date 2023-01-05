4d06e6d1417d8411fe557413e96e03e83a805c99
Halid Erdur <haliderdur@gmail.com>
2023-01-05 17:27:56 +0000

    Properties of Interface

---
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -0,0 +1,35 @@
+package day45_Abstraction;
+
+public interface PropertiesOfInterface {
+
+    int a = 100; // static and final by default
+    static int b = 200; // final by default
+
+    /*
+    public PropertiesOfInterface(int a) {
+        this.a = a;
+    }
+     */
+
+    /*
+    static {
+        b = 100;
+    }
+     */
+
+    /*
+    public void method1() {
+        System.out.println("Instance method");
+    }
+     */
+
+    static void method2() {  // static method
+        System.out.println("Static method");
+    }
+
+    void method3(); // abstract method
+
+    default void method4() { // default method
+        System.out.println("Default method");
+    }
+}
