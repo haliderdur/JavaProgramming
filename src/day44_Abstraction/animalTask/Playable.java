43984398994cafadfc0d5f403c06044b8515d7eb
Halid Erdur <haliderdur@gmail.com>
2023-01-05 17:04:51 +0000

    Zoo Task - Abstraction with Interface Practise

---
+++ src/day44_Abstraction/animalTask/Playable.java
@@ -0,0 +1,10 @@
+package day44_Abstraction.animalTask;
+
+public interface Playable {
+
+    boolean isFriendly = true;  // public, static and final by default
+
+    void play(); // public and abstract by default
+
+
+}
---
+++ src/day44_Abstraction/animalTask/WildAnimal.java
@@ -0,0 +1,9 @@
+package day44_Abstraction.animalTask;
+
+public interface WildAnimal {
+
+    boolean isWild = true; // public, static and final by default
+
+    void hunt(); // public and abstract by default
+
+}
